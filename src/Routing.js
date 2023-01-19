2fc20310061e64d4d70c5e2794671fe815822942
Viola Zhao <violaxzhao@gmail.com>
2023-01-18 16:18:06 -0800

    added login page

--- src/Routing.js
+++ src/Routing.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 
 export default function Routing() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<Login/>} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/containers/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../css/Login.css";
+import { Button, Form, FormGroup, Input } from "reactstrap";  
+
+export default function Login() {
+  return (
+    <div className="main">
+      <h1>Log In</h1>
+      <div className="Login-header">
+        <Form>
+          <FormGroup className="login-input">
+            <Input
+              type="text"
+              id="username-input"
+              className="login-input"
+              placeholder="Username"
+            />
+          </FormGroup>
+          <FormGroup className="login-input"> 
+            <Input
+              type="text"
+              id="game-min-rating-input"
+              placeholder="Password"
+            />
+          </FormGroup>
+        </Form>
+        <Button
+            className="Login"
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
