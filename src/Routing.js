9364af2425570f16fe70bf2dd995c38d861b78d9
Viola Zhao <violaxzhao@gmail.com>
2023-01-18 16:22:40 -0800

    added navigation

--- src/Routing.js
+++ src/Routing.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Navigation from "./containers/Navigation";
 
 export default function Routing() {
     return (
         <BrowserRouter>
+            <Navigation></Navigation>
             <Routes>
-                <Route path='/' element={<Login/>} />
+                <Route path='/' element={<Home/>} />
+                <Route path='/login' element={<Login/>} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/containers/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Navigation.css"
+
+export default function Navigation() {
+    return (
+        <div className="nav">
+            <a href="/">Home</a>
+            <a href="/login">Log In</a>
+        </div>
+    );
+}
