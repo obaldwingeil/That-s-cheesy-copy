4fbffb52fddafcf100e65d318ce3af4981e6c69b
acsanti92 <77762625+acsanti92@users.noreply.github.com>
2023-01-24 15:08:12 -0800

    Add files via upload

--- PythonCheese/main.py
+++ PythonCheese/main.py
@@ -1,13 +1,24 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
+from pymongo import MongoClient
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 
-# These dependecies allow us to create the basic structure of our Flask application and connect to MongoDB
+
+# These dependecies alpip3 low us to create the basic structure of our Flask application and connect to MongoDB
 app = Flask(__name__)  # Create the Flask application
+CORS(app)
 # Connect to MongoDB
-app.config["MONGO_URI"] = "mongodb://localhost:27017/cheesydb"
-mongo = PyMongo(app)  # Create a PyMongo object
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/cheesydb"
+client = MongoClient(
+    "mongodb+srv://Armando:Cheesy123@cheesy.1pp17n9.mongodb.net/?retryWrites=true&w=majority")
+db = client.cheesydb
+recipes_collection = db.recipes
+user_collection = db.login
 
+# mongo = PyMongo()  # Create a PyMongo object
+# mongo.init_app(app) // Initialize the PyMongo object with the Flask application
 ### Recipe class that uses ObjectId as its primary key: ###
 
 
@@ -24,47 +35,38 @@ class Recipe:
 ### Recipes ###
 
 
-@app.route("/recipes", methods=["GET"])
+@app.route('/recipes', methods=['GET'])
+def get_recipes():
+    recipes = recipes_collection.find()
+    recipes_list = list(recipes)
+    return dumps(recipes_list)
+
+
 @app.route('/recipe', methods=['POST'])
 def add_recipe():
     recipe = request.json
-    mongo.db.recipes.insert_one(recipe)
-    return jsonify({'message': 'Recipe created successfully'}), 201
-
-# Endpoints for getting all recipes, updating a recipe, and deleting a recipe
+    recipes_collection.insert_one(recipe)
+    return dumps({'message': 'Recipe added successfully'}), 201
 
 
 @app.route('/recipe/<id>', methods=['GET'])
 def get_recipe(id):
-    recipe = mongo.db.recipes.find_one({'_id': ObjectId(id)})
-    return jsonify(recipe), 200
+    recipe = recipes_collection.find_one({'_id': ObjectId(id)})
+    return dumps(recipe), 200
 
 
 @app.route('/recipe/<id>', methods=['PUT'])
 def update_recipe(id):
     recipe = request.json
-    mongo.db.recipes.update_one({'_id': ObjectId(id)}, {'$set': recipe})
-    return jsonify({'message': 'Recipe updated successfully'}), 200
+    recipes_collection.update_one({'_id': ObjectId(id)}, {'$set': recipe})
+    return dumps({'message': 'Recipe updated successfully'}), 200
 
 
 @app.route('/recipe/<id>', methods=['DELETE'])
 def delete_recipe(id):
-    mongo.db.recipes.delete_one({'_id': ObjectId(id)})
-    return jsonify({'message': 'Recipe deleted successfully'}), 200
-
-# Endpoint for getting all of the recipes
+    recipes_collection.delete_one({'_id': ObjectId(id)})
+    return dumps({'message': 'Recipe deleted successfully'}), 200
 
 
-@app.route('/recipes', methods=['GET'])
-def get_all_recipes():
-    recipes = mongo.db.recipes.find()
-    output = []
-    for recipe in recipes:
-        output.append({'title': recipe['title'], 'ingredients': recipe['ingredients'],
-                       'instructions': recipe['instructions'], 'personal_notes': recipe['personal_notes'],
-                       'image': recipe['image'], '_id': str(recipe['_id'])})
-    return jsonify(output), 200
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
