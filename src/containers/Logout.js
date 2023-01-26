9f8046c85fa462cb87b5ca3e3de22b928300a632
Viola Zhao <violaxzhao@gmail.com>
2023-01-26 15:12:26 -0800

    styling for login and logout pages, cheese cursor

--- src/containers/Logout.js
+++ src/containers/Logout.js
@@ -13,7 +13,7 @@ export default function Logout({ handleLogout }) {
 
     return (
         <div className="Logout">
-            <h3>Are you sure you want to log out?</h3>
+            <h3 className="logout-title"><span className="login-cheese">🧀</span> <br/> Are you sure you want to log out?</h3>
             <Button className="logoutButton" onClick={logout}>Log Out</Button>
         </div>
     )
