a64ad64df3a4b8084f68e28549fdcebb46f87a38
Viola Zhao <violaxzhao@gmail.com>
2023-01-25 16:10:31 -0800

    images added to recipe list

--- src/containers/RecipeListItem.js
+++ src/containers/RecipeListItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/RecipeListItem.css";
 
-export default function RecipeListItem({ id, title, ingredients, instructions, user_id }) {
+export default function RecipeListItem({ id, title, image, ingredients, instructions, user_id }) {
 
     const navigate = useNavigate();
     
@@ -13,6 +13,8 @@ export default function RecipeListItem({ id, title, ingredients, instructions, u
     return(
         <div className="RecipeListItem" onClick={handleClick}>
             <div className="title">{title}</div>
+            <img width="100" src={image} />
+            <h3></h3>
         </div>
     )
 
