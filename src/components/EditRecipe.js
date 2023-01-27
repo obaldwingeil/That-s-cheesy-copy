c2447e71f173c8591c5b350939bdd92ca5c45472
Olivia Baldwin <obaldwingeil@oxy.edu>
2023-01-26 16:07:43 -0800

    added youtube link form to add recipe and edit recipe

--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -13,7 +13,9 @@ class EditRecipe extends Component {
             instruction: "",
             ingredients: [],
             instructions: [],
-            image: null
+            image: null,
+            embedId: "",
+            invalidURL: false
         }
         this.onChangeTitle = this.onChangeTitle.bind(this);
         this.onChangeIngredient = this.onChangeIngredient.bind(this);
@@ -22,6 +24,8 @@ class EditRecipe extends Component {
         this._updateIngredients = this._updateIngredients.bind(this);
         this._updateInstructions = this._updateInstructions.bind(this);
         this.editRecipe = this.editRecipe.bind(this);
+        this._getEmbedId = this._getEmbedId.bind(this);
+        this._cancel = this._cancel.bind(this);
     }
 
     componentDidMount() {
@@ -36,9 +40,10 @@ class EditRecipe extends Component {
                 title: data.title,
                 ingredients: data.ingredients,
                 instructions: data.instructions,
-                image: data.image
+                image: data.image,
+                embedId: data.embedId
             });
-            console.log(id, data.title, data.ingredients, data.instructions, data.image)
+            // console.log(id, data.title, data.ingredients, data.instructions, data.image)
         })
         .catch (function(error) {
             console.log(error)
@@ -73,9 +78,17 @@ class EditRecipe extends Component {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({title: this.state.title, ingredients: this.state.ingredients, instructions: this.state.instructions, image: this.state.image})
+            body: JSON.stringify({
+                title: this.state.title, 
+                ingredients: this.state.ingredients, 
+                instructions: this.state.instructions, 
+                image: this.state.image,
+                embedId: this.state.embedId
+            })
         })
-        .then (this.setState({ redirect: true}));
+        .then ((response) => response.json())
+        .then ((data) => {console.log(data)})
+        this.props.router.navigate(`/recipe/${this.state.recipe_id}`);
     }
   
     _updateIngredients() {
@@ -90,6 +103,32 @@ class EditRecipe extends Component {
         new_instructions.push(this.state.instruction);
         this.setState({instructions: new_instructions});
         console.log(new_instructions);
+    }
+
+    _getEmbedId(event) {
+        if (event === "") {
+            this.setState({
+                embedId: "",
+                invalidURL: false
+            });
+        } else {
+            const regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/
+            const match = event.match(regExp);
+            if (match === null) {
+                this.setState({
+                    invalidURL: true
+                });
+            } else {
+                this.setState({
+                    embedId: match[1],
+                    invalidURL: false
+                });
+            }
+        }
+    }
+
+    _cancel() {
+        this.props.router.navigate(`/recipe/${this.state.recipe_id}`);
     }
 
     render() {
@@ -137,6 +176,20 @@ class EditRecipe extends Component {
                         <br></br>
                         <h3>Select Image</h3>
                         <input type="file" name="myImage" onChange={this.onChangeImage} />
+                        <div className="youtubeInput container">
+                            {this.state.invalidURL ? <div className="invalidURL">
+                                Invalid URL
+                            </div> : <div/>}
+                            <FormGroup className="AddRecipe-input">
+                            <Input
+                                type="text"
+                                id="AddYoutubeURL-input"
+                                placeholder="Youtube URL"
+                                onBlur={(e) => this._getEmbedId(e.target.value)}
+                                defaultValue={this.state.embedId === "" ? "" : "https://youtu.be/" + this.state.embedId}
+                            />
+                            </FormGroup>
+                        </div>
                         <br></br>
                         {ingredient_map}
                         <br></br>
@@ -177,6 +230,9 @@ class EditRecipe extends Component {
                         </FormGroup>
                     </Form>
                     <br></br>
+                    <Button className="cancel_button" onClick={this._cancel}>
+                        Cancel
+                    </Button>
                     <Button onClick={() => this.editRecipe(this.state.recipe_id)} className="EditRecipe">
                         Update Recipe
                     </Button>
