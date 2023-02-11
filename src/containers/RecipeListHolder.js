da498b520b7940f7e2bf8e724a3877e52b7756d9
Olivia Baldwin <65006746+obaldwingeil@users.noreply.github.com>
2023-02-10 16:21:21 -0800

    testing change fetch endpoint to 0.0.0.0

--- src/containers/RecipeListHolder.js
+++ src/containers/RecipeListHolder.js
@@ -30,7 +30,7 @@ class RecipeListHolder extends Component {
                 this._getRecipes(`http://127.0.0.1:8000/favorites/${user_id}`);
             }
         } else {
-            this._getRecipes('http://127.0.0.1:8000/recipes');
+            this._getRecipes('http://0.0.0.0:8000/recipes');
         }
     }
 
