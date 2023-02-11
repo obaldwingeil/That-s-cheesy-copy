23ef1595582fb35a4d8b1238848e4947f2b082c5
Olivia Baldwin <65006746+obaldwingeil@users.noreply.github.com>
2023-02-10 17:07:58 -0800

    personalized test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { store } from './redux/redux';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders title', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const linkElement = screen.getByText("That's Cheesy");
   expect(linkElement).toBeInTheDocument();
 });
